7987171e5d6a6445468f2ed08ac661a1d5214767
Sergei <claymoreus@gmail.com>
2019-09-24 14:53:06 +0300

    figth block

--- Java/Clicker/src/BaseScript.java
+++ Java/Clicker/src/BaseScript.java
@@ -3,6 +3,11 @@ public class BaseScript {
 
 	public static void main(String[] args) {
 		System.out.println("ITS ALIVE!");
+		int a = 30;
+		int b = 17;
+		String name = "Rex";
+		int age = a+b;
+		System.out.println("Name " + name + " Age " + age);
 
 	}
 
