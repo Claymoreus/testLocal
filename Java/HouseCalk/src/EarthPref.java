182f6a2e9642ab64b93291b981c114a1fb685cf7
Sergei <claymoreus@gmail.com>
2019-09-27 21:30:35 +0300

    fundamentTest

--- Java/HouseCalk/src/EarthPref.java
+++ Java/HouseCalk/src/EarthPref.java
@@ -1,32 +1,49 @@
 import java.util.Scanner;
 
+class EarthPrefMod {
+	
+	int hiF;
+	String houseName;
+	String numberTer;
+	int grWater;
+	int grFrozen;
+	int grGostReg;
+}	
 public class EarthPref {
-	public static int hiF;
-	//public static void main(String[] args) {
-	public EarthPref(Integer hiF) {	
+	//private String numberTer;
+	public static void main (String[] args) {	
+	//public EarthPref() {	
 		Scanner scan = new Scanner(System.in);
+
+		EarthPrefMod territoryNum001 = new EarthPrefMod();
+		System.out.println("Enter name of house (project name):");
+		territoryNum001.houseName = scan.next();
+		territoryNum001.numberTer = territoryNum001.houseName + "001";
+		System.out.println("Number of territory is :" + territoryNum001.numberTer);
+		
 		try { 
 			System.out.println("Enter in cm water level on territory (ignore '-')");
-			int grWater = scan.nextInt();
+			territoryNum001.grWater = scan.nextInt();
 			System.out.println("Enter in cm frozen level on territory (ignore '-')");
-			int grFrozen = scan.nextInt();
+			territoryNum001.grFrozen = scan.nextInt();
 			System.out.println("Enter in cm parametr for fundament on region in the GOST/SP/ISO doc (ignore '-')");
-			int grGostReg = scan.nextInt();
+			territoryNum001.grGostReg = scan.nextInt();
 			scan.close();
-			hiF = 0;
-			if (hiF < grWater) {
-				hiF = grWater;
-			} if (hiF < grFrozen) {
-				hiF = grFrozen;
-			} if (hiF < grGostReg) {
-				hiF = grGostReg;
-			} else 
+			territoryNum001.hiF = 0;
+			if (territoryNum001.hiF < territoryNum001.grWater) {
+				territoryNum001.hiF = territoryNum001.grWater;
+			} if (territoryNum001.hiF < territoryNum001.grFrozen) {
+				territoryNum001.hiF = territoryNum001.grFrozen;
+			} if (territoryNum001.hiF < territoryNum001.grGostReg) {
+				territoryNum001.hiF = territoryNum001.grGostReg;
+			} else if (territoryNum001.hiF == 0) {
 			System.out.println("Uncorrect!");
+			}
 		}	catch (Exception e) {
 			System.out.println("Uncorrect type of data");
 		}
-		System.out.println("Min heigth of fundament is: " + hiF);
-		return;
+		System.out.println("Min heigth of fundament - " + territoryNum001.numberTer + " is: " + territoryNum001.hiF);
+		//return;
 		
 	}
 }
--- Java/HouseCalk/src/FundCalk.java
+++ Java/HouseCalk/src/FundCalk.java
@@ -1,37 +1,34 @@
 import java.util.Scanner;
 
 public class FundCalk {
-	
+	public static int hiF;
 	public static void main(String[] args) {
+		EarthPref earthPref = new EarthPref();
+		hiF = earthPref.hiF;
+		System.out.println(earthPref.hiF);
 		
-		int x = 0;
-		//EarthPref(hiF);
-		EarthPref eF = new EarthPref();
-		System.out.println(eF);
 	}
 	
 }
 
-class EarthPref {
-	//private int hiF;
+/*class EarthPref {
+	private int hiF;
 	public EarthPref() {
 		
 		Scanner scan = new Scanner(System.in);
 		try { 
 			System.out.println("Enter in cm water level on territory (ignore '-')");
-			
 			int grWater = scan.nextInt();
 			System.out.println("Enter in cm frozen level on territory (ignore '-')");
 			int grFrozen = scan.nextInt();
 			System.out.println("Enter in cm parametr for fundament on region in the GOST/SP/ISO doc (ignore '-')");
 			int grGostReg = scan.nextInt();
 			scan.close();
-			int hiF = 0;
+			hiF = 0;
 			if (hiF < grWater) {
 				hiF = grWater;
 			} if (hiF < grFrozen) {
 				hiF = grFrozen;
-				
 			} if (hiF < grGostReg) {
 				hiF = grGostReg;
 			} else 
@@ -39,8 +36,8 @@ class EarthPref {
 		}	catch (Exception e) {
 			System.out.println("Uncorrect type of data");
 		}
-		//return;
+		return;
 		
 	}
 	
-}
+}*/
